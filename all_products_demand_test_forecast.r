f5ec3626be7b82858c6aaeabebc15563eaf71330
egood13 <elliott.good13@gmail.com>
2018-08-22 22:35:15 -0400

    Update all_products_demand_test_forecast.r

--- all_products_demand_test_forecast.r
+++ all_products_demand_test_forecast.r
@@ -12,54 +12,7 @@ library("zoo")
 library("dplyr")
 
 
-gtinsQuery = "WITH captured_orders AS (
-	SELECT c.capture_year
-,c.capture_month
-,c.product_style_num || '-' || c.product_color || '-' || c.SIZE || '-' || c.gtin AS style_name 
-,SUM(quantity) AS quantity
-FROM (
-SELECT DATE_PART(YEAR, so.capture_date) as capture_year
-,DATE_PART(MONTH, so.capture_date) as capture_month
-,DATE_PART(WEEK, so.capture_date) AS capture_week
-,sl.gtin
-,sl.product_name
-,sl.product_style_num
-,sl.SIZE
-,INITCAP(sl.color_name) AS product_color
-,sl.printer_id
-,p.name AS printer_name
-,sl.product_brand
-,sl.product_class
-,pg.name AS product_group
-,sl.quantity
-FROM rollups.super_orders so
-JOIN rollups.super_line_items sl ON sl.order_id = so.order_id
-JOIN printers p ON sl.printer_id = p.id
-JOIN product_groups pg ON sl.product_group_id = pg.id
-WHERE so.order_state_id = 3
-AND EXTRACT (YEAR FROM so.capture_date) >= EXTRACT(YEAR FROM CURRENT_DATE) - 2
-AND so.capture_date <= CURRENT_DATE - EXTRACT(DAY FROM CURRENT_DATE)
-) c
-GROUP BY 1,2,3
-)
-SELECT d.num_year AS capture_year
-,d.num_month AS capture_month
-,UPPER(d.style_name) as style_name
-,CASE WHEN c.quantity IS NULL THEN 0 ELSE c.quantity END AS quantity 
-FROM ( -- generates list of month/year dates for each style_name
-SELECT DISTINCT d.num_year
-,d.num_month
-,prod_list.style_name
-FROM d_date d
-CROSS JOIN (SELECT DISTINCT style_name FROM captured_orders) prod_list
-WHERE EXTRACT(YEAR FROM dateobj) >= EXTRACT(YEAR FROM CURRENT_DATE) - 2
-AND dateobj <= CURRENT_DATE - EXTRACT(DAY FROM CURRENT_DATE)
-ORDER BY 1,2
-) d
-LEFT JOIN captured_orders c ON d.num_year = c.capture_year
-AND d.num_month = c.capture_month
-AND d.style_name = c.style_name
-ORDER BY 3,1,2"
+gtinsQuery = sqlQuery
 
 # get list of gtin/style_name on auto replen
 mapping <- read.csv("auto_replen_gtins.txt")
